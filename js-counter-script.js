78ef8dec776fc568c4dee506241db0373deb05ae
MegBuendia <megitako@gmail.com>
2017-03-21 21:41:10 +0800

    Update js-counter-script.js

--- js-counter-script.js
+++ js-counter-script.js
@@ -2,6 +2,9 @@ function modify_qty(val) {
     var qty = document.getElementById('qty').value;
     var new_qty = parseInt(qty,10) + val;
 
+    System.out.print("Enter something:");
+    String input = System.console().readLine();
+    
     if (new_qty i = 0; i < number.Count(); i++)
     }
     
