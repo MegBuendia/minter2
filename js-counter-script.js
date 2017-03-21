f7a8f69e38ce00afbb66db25a25994803ced66c6
MegBuendia <megitako@gmail.com>
2017-03-21 21:28:04 +0800

    Create js-counter-script.js

---
+++ js-counter-script.js
@@ -0,0 +1,10 @@
+function modify_qty(val) {
+    var qty = document.getElementById('qty').value;
+    var new_qty = parseInt(qty,10) + val;
+
+    if (new_qty i = 0; i < number.Count(); i++)
+    }
+    
+    document.getElementById('qty').value = new_qty;
+    return new_qty;
+}
